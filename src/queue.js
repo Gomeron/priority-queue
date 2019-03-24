4880cb2e9d9c1e43bc1dc99293c9f0ed9ceafad2
Gomeron <zhuckkirill@yandex.ru>
2019-03-24 19:46:22 +0300

    Add files via upload

---
+++ src/queue.js
@@ -0,0 +1,28 @@
+const MaxHeap = require('./max-heap.js');
+
+class PriorityQueue {
+    constructor(maxSize) {
+        this.maxSize = maxSize || 30
+        this.heap = new MaxHeap
+    }
+
+    push(data, priority) {
+        if (this.heap.size() == this.maxSize) throw "error"
+        this.heap.push(data, priority)
+    }
+
+    shift() {
+        if (this.isEmpty()) throw "error"
+        return this.heap.pop()
+    }
+
+    size() {
+        return this.heap.size()
+    }
+
+    isEmpty() {
+        return this.heap.isEmpty()
+    }
+}
+
+module.exports = PriorityQueue;
